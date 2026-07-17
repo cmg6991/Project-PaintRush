4d751b45168136aee3c16c601ca616e214f79a31
cmg6991 <110959882+cmg6991@users.noreply.github.com>
2026-07-18 00:26:14 +0900

    리스타트 구현

---
+++ Assets/Scripts/Collider/DeadZone.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeadZone : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+
+        //UIManager.Instance.ShowRestartUI();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
